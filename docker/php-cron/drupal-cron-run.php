3ab68afd40f566b2844a16745e2abbc9d7537a16
Aaron McHale <aaron@mchale.me>
2021-02-26 14:10:09 +0000

    Drupal Cron Script: improved output and error handling

--- docker/php-cron/drupal-cron-run.php
+++ docker/php-cron/drupal-cron-run.php
@@ -12,26 +12,58 @@
  * @see https://www.drupal.org/project/ideas/issues/3089277
  */
 
+// Open streams to Standard Out and Standard Error
+$stdout = fopen('php://stdout', 'w');
+$stderr = fopen('php://stderr', 'w');
+
+fwrite($stdout, "Running Drupal Cron script.\n");
+
+use Drupal\Core\Database\ConnectionNotDefinedException;
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
 
 // @todo figure out path from environment maybe?
 $app_root = '/var/www/app/web';
 
+if (!is_dir($app_root)) {
+  fwrite($stderr, "Unable to run Drupal Cron: Expected path $app_root which does not exist in the Cron Container, aborting.\n");
+  exit(1);
+}
+
 // chdir to the app_root directory, ensures Drupal root-relative
 // paths can be resolved (otherwise SQLite connections fail).
 chdir($app_root);
 
-$autoloader = require_once 'autoload.php';
+$autoload_filename = 'autoload.php';
+
+if (!file_exists($autoload_filename)) {
+  fwrite($stderr, "Unable to run Drupal Cron: Could not find $autoload_filename in $app_root, aborting.\n");
+  exit(1);
+}
+
+$autoloader = require_once $autoload_filename;
 
 // Initialise a Request and Kernel
 $request = Request::createFromGlobals();
 $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
-$kernel->boot();
+try {
+  $kernel->boot();
+}
+catch (ConnectionNotDefinedException $ex) {
+  fwrite($stderr, "Unable to run Drupal Cron: Could not initialise a database connection, Drupal is probably not installed (\Drupal\Core\DrupalKernel::boot() threw exception \Drupal\Core\Database\ConnectionNotDefinedException).\n");
+  exit(1);
+}
 
   /** @var \Drupal\Core\CronInterface $cron */
 $cron = $kernel->getContainer()->get('cron');
 
 // Run Cron, essentially mimicking what the "/cron" URL does
 // @see \Drupal\system\CronController::run
-$cron->run();
+if ($cron->run()) {
+  fwrite($stdout, "Drupal Cron run completed.\n");
+  exit(0);
+}
+else {
+  fwrite($stderr, "Unable to run Drupal Cron: Failed while executing \Drupal\Core\CronInterface::run().\n");
+  exit(1);
+}
